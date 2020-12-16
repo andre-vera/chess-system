fe23fcc87c7ac50fe197b787ca9470261f3a558f
Andre Vera <andreluizvera@hotmail.com>
2020-12-16 13:08:18 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -9,25 +11,34 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
-
-	public static void main(String[] args) {
 	
+	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	ChessMatch chessmatch = new ChessMatch();
+	List<ChessPiece> captured = new ArrayList<>();
 	
 	while (true) {
 		try {
 		UI.clearScreen();
-		UI.printBoard(chessmatch.getPieces());
+		UI.printMatch(chessmatch, captured);
 		System.out.println();
 		System.out.print("Coordenadas da peca selecionada: ");
 		ChessPosition source = UI.readChessPosition(sc);
+		
+		boolean[][] possibleMoves = chessmatch.possibleMoves(source);
+		UI.clearScreen();
+		UI.printBoard(chessmatch.getPieces(), possibleMoves);
+		
 		System.out.println();
 		
 		System.out.print("Coordenadas da posicao-alvo: ");
 		ChessPosition target = UI.readChessPosition(sc);
 		
 		ChessPiece capturedPiece = chessmatch.performChessMove(source, target);
+		
+		if (capturedPiece != null) {
+			captured.add(capturedPiece);
+		}
 		}
 		catch (ChessException e) {
 			System.out.println(e.getMessage());
